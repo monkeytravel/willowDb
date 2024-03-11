f0a829c856b8e9c71000f34de06a5a4785329a39
Cameron Perrier <20784921+monkeytravel@users.noreply.github.com>
2024-03-10 18:27:27 -0700

    updated release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
     name = 'willowDb',
     packages = ['willowDb'],
-    version = '0.1',
+    version = '0.1.1',
     license='MIT',
     description = 'A document database as a python module',
     author = 'Cameron Perrier',
